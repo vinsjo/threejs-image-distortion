af2f370097e888fa7446c8ed2d973a4a998f511f
Vincent Sjögren <vincentsjogren@imacsomeleonore.lan>
2022-04-27 14:21:48 +0200

    updated preloadImages function

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,22 +2,19 @@
  * @param {String[]} urls
  */
 async function preloadImages(urls) {
-	const preload = urls.map(
-		(url) =>
-			new Promise((resolve) => {
-				const img = $(`<img src="${url}">`);
-				img.on('load', function () {
-					resolve(this);
-				}).on('error', () => resolve(false));
-				if (img.get(0).complete) img.trigger('load');
-			})
+	const output = await Promise.all(
+		urls.map(
+			(url) =>
+				new Promise((resolve) => {
+					const img = $(`<img src="${url}">`);
+					img.on('load', function () {
+						resolve(this);
+					}).on('error', () => resolve(false));
+					if (img.get(0).complete) img.trigger('load');
+				})
+		)
 	);
-	const output = [];
-	for await (const img of preload) {
-		if (!img) continue;
-		output.push(img);
-	}
-	return output;
+	return output.filter((img) => !!img);
 }
 
 export { preloadImages };
